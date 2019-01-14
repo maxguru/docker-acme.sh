822630b0c7b6383f54e445c5487a4a0e6d63ee73
maxguru <alex@maximum.guru>
2019-01-13 16:37:55 -0800

    Saving environment variables so they are accessible in cron jobs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# save environment variables so they are accessible in cron jobs
+env | grep -v "^HOME=" > /etc/environment
+
 if [ "$1" = 'cron' -a "$2" = '-f' ]; then
     echo "Cron job is running to renew all the certs..."
 fi
